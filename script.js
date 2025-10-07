b74fee6605fd5305d5c5473e70f586ff1089c3cf
Raham Kalhoro <rahamkalhoro@gamil.com>
2025-10-07 19:50:30 +0500

    My second chellange of the JavaScript

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-let output = [];
-function sumFunction(sum) {
-  let storing = [];
-  for (let i = 1; i <= sum; i++) {
-    storing.push(i);
-  }
-  let value = storing.reduce((prev, curr) => {
-    return prev * curr;
-  });
-  output.push(value);
+function calculateStarNumber(n) {
+  let value = 6 * n * n;
+  return value;
 }
 
-sumFunction(10);
-
-output.forEach((items) => {
-  for (const chars of items) {
-    console.log(chars);
-  }
-});
-
-// for Understanding how forEach works?
-const myArray = ["Apple"];
-
-myArray.forEach((item) => {
-  for (const char of item) {
-    console.log(char);
-  }
-});
+calculateStarNumber(3);
