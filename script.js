54d5ba608196d178ca0d30444f3d5edbf6a6745c
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-10-09 16:59:43 +0500

    I completed the 5th challenge

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function calculateStarNumber(n) {
-  let value = 6 * n * n;
-  return value;
+function factorialDigitSum(sum) {
+  // calculate factorial
+  let factorial = 1n;
+  // BigInt for handle large numbers
+  for (let i = 1; i <= sum; i++) {
+    factorial *= BigInt(i);
+  }
+  // convert factorial toString to sum the digits
+  let factorialStr = factorial.toString();
+  let Sum = 0;
+  for (let digits of factorialStr) {
+    Sum += parseInt(digits);
+  }
+  return Sum;
 }
 
-calculateStarNumber(3);
+console.log(factorialDigitSum(4));
+// This code is solving the challenge of the factorialCounting?
