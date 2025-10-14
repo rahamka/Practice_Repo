423424c367fb21b7274e79b8bdb9e86605e4939d
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-10-14 09:24:28 +0500

    Completed Chellange 2

--- script.js
+++ script.js
@@ -10,6 +10,22 @@ function findUnion(arr1, arr2) {
   return uniqueNumbers;
 }
 
-// if(navigator.onLine){
-
-// }
+// Chellange 3 Coins = 50
+{
+  // my code
+  function addToNestedArray(arr, num) {
+    let value1 = arr[0];
+    value1.push(num);
+    let value2 = arr[1];
+    value2.push(num);
+    let value3 = arr[2];
+    value3.push(num);
+    const result = [value1, value2, value3];
+    return result;
+  }
+  // correct code
+  function addToNestedArray(arr, num) {
+    arr.forEach((subArr) => subArr.push(num));
+    return arr;
+  }
+}
