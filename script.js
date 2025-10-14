f40b1e187498eaeff765bf9f23848d709ac9143c
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-10-14 21:47:09 +0500

    Understand how to write function in objects.

--- script.js
+++ script.js
@@ -50,3 +50,21 @@ function findUnion(arr1, arr2) {
 }
 
 // LeetCode Chellange
+
+var createCounter = function (init) {
+  let currentValue = init;
+  return {
+    increment: function () {
+      currentValue += 1;
+      return currentValue;
+    },
+    decrement: function () {
+      currentValue -= 1;
+      return currentValue;
+    },
+    reset: function () {
+      currentValue = init;
+      return currentValue;
+    },
+  };
+};
