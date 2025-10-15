8f4906031d84a4ea0e48ff5eaf49a431f9526191
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-10-16 04:22:43 +0500

    can't done this Js Question.

--- script.js
+++ script.js
@@ -1,84 +1,8 @@
-// Chellange 2 Coins = 50
-function findUnion(arr1, arr2) {
-  let value1 = arr1;
-  let value2 = arr2;
-  let result = arr1.concat(arr2);
-  function removeDuplicatesWithSet(arr) {
-    return [...new Set(arr)];
+function hideCreditCard(cardNumber) {
+  for (let items in cardNumber) {
+    console.log(items);
   }
-  const uniqueNumbers = removeDuplicatesWithSet(result);
-  return uniqueNumbers;
+  return console.log(`************${cardNumber}`);
 }
 
-// Chellange 3 Coins = 50
-{
-  // my code
-  function addToNestedArray(arr, num) {
-    let value1 = arr[0];
-    value1.push(num);
-    let value2 = arr[1];
-    value2.push(num);
-    let value3 = arr[2];
-    value3.push(num);
-    const result = [value1, value2, value3];
-    return result;
-  }
-  // correct code
-  function addToNestedArray(arr, num) {
-    arr.forEach((subArr) => subArr.push(num));
-    return arr;
-  }
-}
-
-// Chellange 4 Coins = 50
-{
-  function isPrime(n) {
-    if (n <= 1) return false;
-    if (n == 2) return true;
-    if (n % 2 == 0) return false;
-    for (let i = 3; i * i <= n; i += 2) {
-      if (n % i == 0) return false;
-    }
-    return true;
-  }
-
-  function formsWagstaffPrime(p) {
-    if (!isPrime(p)) return false;
-    let wagstaff = (Math.pow(2, p) + 1) / 3;
-    return isPrime(wagstaff);
-  }
-}
-
-// LeetCode Chellange
-
-var createCounter = function (init) {
-  let currentValue = init;
-  return {
-    increment: function () {
-      currentValue += 1;
-      return currentValue;
-    },
-    decrement: function () {
-      currentValue -= 1;
-      return currentValue;
-    },
-    reset: function () {
-      currentValue = init;
-      return currentValue;
-    },
-  };
-};
-
-// // counting character
-// let getElement = document.getElementById("heading");
-// let array = getElement.innerText;
-// let count = [];
-// // console.log(array);
-// for (let i = 0; i <= array.length; i++) {
-//   if (array[i] !== "") {
-//     count.push(array[i]);
-//   }
-// }
-
-// getElement.innerHTML += ` Characters = ${count.length}`;
-// // How to remove the white spaces from count?
+hideCreditCard(1234567890123456);
