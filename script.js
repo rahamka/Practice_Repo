7e0aab2df9c9ec4fd7ee26eada56be1f6324e832
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-10-17 04:58:01 +0500

    Currently Solving 22 Problem

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function hideCreditCard(cardNumber) {
 }
 
 hideCreditCard(1234567890123456);
+
+// 22 Problem on Programiz Pro
+function findMissingLetter(str) {
+  let value = "abcdefghijklmnopqrstuvwxyz";
+  let textValue = str;
+  let array = [];
+  textValue = textValue[0];
+  array.push(textValue);
+  textValue = str.charAt(str.length - 1);
+  array.push(textValue);
+  let starting = array[0];
+  let ending = array[1];
+  for (let i = value[starting]; i <= value[ending]; i++) {
+    console.log(i);
+  }
+}
+findMissingLetter("abcdf");
