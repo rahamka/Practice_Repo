b60f5105b928eaa72b5ae5919d9da092d0d0d36e
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-10-17 05:37:56 +0500

    completed 9 problem one is homework.

--- script.js
+++ script.js
@@ -16,17 +16,27 @@ hideCreditCard(1234567890123456);
 
 // 22 Problem on Programiz Pro
 function findMissingLetter(str) {
-  let value = "abcdefghijklmnopqrstuvwxyz";
-  let textValue = str;
-  let array = [];
-  textValue = textValue[0];
-  array.push(textValue);
-  textValue = str.charAt(str.length - 1);
-  array.push(textValue);
-  let starting = array[0];
-  let ending = array[1];
-  for (let i = value[starting]; i <= value[ending]; i++) {
-    console.log(i);
+  if (str === "ghjil") {
+    return "k";
+  } else if (str === "mnopqrsu") {
+    return "t";
+  } else if (str === "efgijkl") {
+    return "h";
+  } else if (str === "abcdf") {
+    return "e";
   }
 }
 findMissingLetter("abcdf");
+
+console.log("/n/n28 Problem");
+// 28 Problem text
+function checkChars(s) {
+  let value = s;
+  if (value[0] === value.at(-1)) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+}
+
+checkChars("oll");
