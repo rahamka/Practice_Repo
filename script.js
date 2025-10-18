4f30d3c18b27675537160b7c67c9d4a45ab3a5d5
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-10-18 15:25:38 +0500

    I've makes new simple project.

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ let box1 = document.querySelector("#box1");
 let container = document.querySelector("#container");
 if (box1.animate) {
   setTimeout(() => {
-    box1.style.position = "relative";
-    box1.style.left = "-0vh";
     container.style.display = "flex";
     container.style.justifyContent = "center";
   }, 1010);
