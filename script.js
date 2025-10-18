c01ea0850882ef3be7dfba0ce20c63e0897741af
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-10-18 09:28:21 +0500

    Created a Responsive Project.

--- script.js
+++ script.js
@@ -1,44 +1,5 @@
-// One Problem
-function hideCreditCard(cardNumber) {
-  let str = cardNumber.toString();
-  let value = ""; // Initialize value as an empty string
-  for (let i = 0; i < str.length; i++) {
-    if (i < 12) {
-      value += "*";
-    } else {
-      value += str[i];
-    }
-  }
-  return value;
-}
-
-hideCreditCard(1234567890123456);
-
-// 22 Problem on Programiz Pro
-function findMissingLetter(str) {
-  if (str === "ghjil") {
-    return "k";
-  } else if (str === "mnopqrsu") {
-    return "t";
-  } else if (str === "efgijkl") {
-    return "h";
-  } else if (str === "abcdf") {
-    return "e";
+function repeatVowels(str) {
+  if (str == "A" || str == "E" || str == "i" || str == "o" || str == "u") {
+    // str+=""
   }
 }
-findMissingLetter("abcdf");
-
-console.log("/n/n28 Problem");
-// 28 Problem text
-function checkChars(s) {
-  let value = s;
-  if (value[0] === value.at(-1)) {
-    console.log("true");
-  } else {
-    console.log("false");
-  }
-}
-
-checkChars("oll");
-
-// please complete 15 a day.
