594c45db0860e8eb9cc82c51a0ae9db9bd3c4d6b
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-10-18 13:29:40 +0500

    I makes beautiful animation box.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-function repeatVowels(str) {
-  if (str == "A" || str == "E" || str == "i" || str == "o" || str == "u") {
-    // str+=""
-  }
+let box1 = document.querySelector("#box1");
+let container = document.querySelector("#container");
+if (box1.animate) {
+  setTimeout(() => {
+    box1.style.position = "relative";
+    box1.style.left = "-0vh";
+    container.style.display = "flex";
+    container.style.justifyContent = "center";
+  }, 1010);
 }
