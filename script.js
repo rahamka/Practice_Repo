9b76e3ed580bec4f32b202c5fc3fe2f208ff69bf
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-10-19 14:25:58 +0500

    I make simple practice container.

--- script.js
+++
@@ -1,8 +0,0 @@
-let box1 = document.querySelector("#box1");
-let container = document.querySelector("#container");
-if (box1.animate) {
-  setTimeout(() => {
-    container.style.display = "flex";
-    container.style.justifyContent = "center";
-  }, 1010);
-}
