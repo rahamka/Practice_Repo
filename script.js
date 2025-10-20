c7d71a037f7e4f44c7c12104adab050eb3189c5c
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-10-20 09:35:06 +0500

    Build Simple Project.

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ let element = document.getElementById("box-1");
 if (element) {
   element.addEventListener("animationend", function () {
     setTimeout(() => {
-      element.classList.add("box1");
+      // element.style.animation = "rotateAnimate 2s ease 0s normal";
+      // element.style.@KeyframeEffect{}
+      // How to apply @KeyframeEffect in JavaScript?
     }, 2000);
   });
 }
+setTimeout(() => {
+  const newElement = document.createElement("div");
+  newElement.innerText = "Created By Raham Kalhoro.";
+  document.body.append(newElement);
+}, 2000);
+
+setTimeout(() => {
+  const newElement2 = document.createElement("div");
+  newElement2.innerText = "Thanks Regards Raham.";
+  document.body.append(newElement2);
+}, 2100);
