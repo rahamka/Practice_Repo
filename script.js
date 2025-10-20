9c3f3e74d030789ee4fabb0dc280beacb2bb811c
Raham Kalhoro <rahamkalhoro@gmail.com>
2025-10-20 12:59:17 +0500

    Build layout like a stop-watch and sliding page.

--- script.js
+++ script.js
@@ -12,13 +12,118 @@ setTimeout(() => {
       },
     ],
     {
-      duration: 999,
+      duration: 450,
       delay: 0,
-      iterations: 1,
+      iterations: 2,
     }
   );
 }, 1100);
 
 setTimeout(() => {
   element.style.borderRadius = "50%";
-}, 1200);
+}, 2000);
+
+let result = null;
+const displayElement = document.createElement("div");
+displayElement.style.fontSize = "30px";
+displayElement.style.fontFamily =
+  "Segoe UI, Tahoma, Geneva, Verdana, sans-serif ";
+
+setTimeout(() => {
+  displayElement.innerText = 1;
+  document.body.append(displayElement);
+}, 50);
+
+setTimeout(() => {
+  displayElement.innerText = 2;
+  document.body.append(displayElement);
+}, 100);
+
+setTimeout(() => {
+  displayElement.innerText = 3;
+  document.body.append(displayElement);
+}, 150);
+
+setTimeout(() => {
+  displayElement.innerText = 5;
+  document.body.append(displayElement);
+}, 200);
+
+setTimeout(() => {
+  displayElement.innerText = 6;
+  document.body.append(displayElement);
+}, 250);
+
+setTimeout(() => {
+  displayElement.innerText = 7;
+  document.body.append(displayElement);
+}, 300);
+
+setTimeout(() => {
+  displayElement.innerText = 8;
+  document.body.append(displayElement);
+}, 350);
+
+setTimeout(() => {
+  displayElement.innerText = 9;
+  document.body.append(displayElement);
+}, 400);
+
+setTimeout(() => {
+  displayElement.innerText = 10;
+  document.body.append(displayElement);
+}, 450);
+
+setTimeout(() => {
+  displayElement.innerText = 11;
+  document.body.append(displayElement);
+}, 500);
+
+setTimeout(() => {
+  displayElement.innerText = 12;
+  document.body.append(displayElement);
+}, 550);
+
+setTimeout(() => {
+  displayElement.innerText = 13;
+  document.body.append(displayElement);
+}, 600);
+
+setTimeout(() => {
+  displayElement.innerText = 14;
+  document.body.append(displayElement);
+}, 650);
+
+setTimeout(() => {
+  displayElement.innerText = 15;
+  document.body.append(displayElement);
+}, 700);
+
+setTimeout(() => {
+  displayElement.innerText = 16;
+  document.body.append(displayElement);
+}, 750);
+
+setTimeout(() => {
+  displayElement.innerText = 17;
+  document.body.append(displayElement);
+}, 800);
+
+setTimeout(() => {
+  displayElement.innerText = 18;
+  document.body.append(displayElement);
+}, 850);
+
+setTimeout(() => {
+  displayElement.innerText = 19;
+  document.body.append(displayElement);
+}, 900);
+
+setTimeout(() => {
+  displayElement.innerText = 20;
+
+  document.body.append(displayElement);
+}, 1000);
+
+// Convert this largest code into simple code
+// Build Stop-Watch to upload the project on linkedIn?
